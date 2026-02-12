c6b2088a18a0ff77588b20570cca77b7ea9e3c96
cleber <lucleber13@hotmail.com>
2026-02-12 22:08:01 +0000

    Initial monorepo setup

---
+++ backend/management_server/src/test/java/com/cbcode/management_server/ManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbcode.management_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
